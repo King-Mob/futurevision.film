4dd6a31826bf721b8fd2f8dd1d8fd72873612f79
John Evans <38507954+King-Mob@users.noreply.github.com>
2025-01-14 15:05:53 +0000

    add desciption behind

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 //place text in circles
 
 const circleRotations = [-45, 90, 45];
+//const circleRotations = [0, 0, -45];
+
+const enter = () => {
+    const lockContainer = document.getElementById("lock-container");
+    lockContainer.style.animation = "inAndThrough 5s forwards";
+
+    setTimeout(() => {
+        lockContainer.style.display = "none";
+    }, 5000)
+
+    const descriptionContainer = document.getElementById("description-container");
+    descriptionContainer.style.display = "flex";
+    descriptionContainer.style.animation = "fromBehind 5s forwards";
+}
 
 const circles = document.getElementsByClassName("circle");
 const enterButton = document.getElementById("enter-button");
@@ -26,15 +40,15 @@ for (const circle of circles) {
         circle.style.transform = `rotate(${circleRotations[circleIndex]}deg)`;
 
         if (circleRotations.reduce((prev, current) => prev + current) === 0) {
-            enterButton.style.display = "block";
+            enterButton.onclick = enter;
+            enterButton.innerHTML = "Enter";
+        }
+        else {
+            enterButton.onclick = () => { };
+            enterButton.innerHTML = "";
         }
     }
 
     index++;
 }
 
-
-enterButton.onclick = () => {
-    const html = document.getElementsByTagName("html")[0];
-    html.style.animation = "inAndThrough 5s";
-}
